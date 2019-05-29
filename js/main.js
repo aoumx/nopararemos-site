6822f7df7293a84f19d90668cc74d063789dac55
Santiago Miqueo <smiqueo@hotmail.com>
2019-05-29 15:55:17 -0500

    agregue link a mail

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
   })
 
   $('.circle').on('click', () => {
-    $('body').css("background-image", "url(textures/texturas_-02.png)");
+    $('body').css("background-image", "url(textures/texturas_-01.png)");
     $('body').css("background-size", 2000 + "px " + 1125 + "px");
   })
 
