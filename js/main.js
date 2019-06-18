84af8790a371c35a6846a38091ca625392598879
Santiago Miqueo <smiqueo@hotmail.com>
2019-06-18 10:20:33 -0500

    saque los href

--- js/main.js
+++ js/main.js
@@ -4,31 +4,31 @@ $(document).ready(() => {
   })
 
   $('.textura-1').on('click', () => {
-    $('body').css("background-image", "url(textures/texturas_-03.jpg");
+    $('body').css("background-image", "url(textures/texturas_-03.png");
     $('body').css("background-size", 2000 + "px " + 1125 + "px");
   })
   $('.textura-2').on('click', () => {
-    $('body').css("background-image", "url(textures/texturas_-05.jpg)");
+    $('body').css("background-image", "url(textures/texturas_-05.png)");
     $('body').css("background-size", 2000 + "px " + 1125 + "px");
   })
   $('.textura-3').on('click', () => {
-    $('body').css("background-image", "url(textures/texturas_-02.jpg)");
+    $('body').css("background-image", "url(textures/texturas_-02.png)");
     $('body').css("background-size", 2000 + "px " + 1125 + "px");
   })
   $('.textura-4').on('click', () => {
-    $('body').css("background-image", "url(textures/texturas_-04.jpg)");
+    $('body').css("background-image", "url(textures/texturas_-04.png)");
     $('body').css("background-size", 2000 + "px " + 1125 + "px");
   })
   $('.textura-5').on('click', () => {
-    $('body').css("background-image", "url(textures/texturas_-01.jpg)");
+    $('body').css("background-image", "url(textures/texturas_-011.png)");
     $('body').css("background-size", 2000 + "px " + 1125 + "px");
   })
   $('.textura-6').on('click', () => {
-    $('body').css("background-image", "url(textures/texturas_-06.jpg)");
+    $('body').css("background-image", "url(textures/texturas_-06.png)");
     $('body').css("background-size", 2000 + "px " + 1125 + "px");
   })
   $('.menu-principal').on('click', () => {
-    $('body').css("background-image", "url(textures/texturas_-01.jpg)");
+    $('body').css("background-image", "url(textures/texturas_-01.png)");
     $('body').css("background-size", 2000 + "px " + 1125 + "px");
   })
 
